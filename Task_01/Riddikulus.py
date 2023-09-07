92597c44fae85eaea91128f553476f5409c7d5c5
ashwinpraveengo <ashwinpraveengo@gmail.com>
2023-09-08 02:09:14 +0530

    add your file

--- Task_01/Riddikulus.py
+++ Task_01/Riddikulus.py
@@ -8,17 +8,11 @@ try:
     userLogin=user['login']
     file=os.path.basename(__file__)
     spell=file.split('.')[0]
-    code="c29tZSBkYXRhIGM"
-
- 
-
+    code="Uh1bnRzbWFuNC9U"
     print(f'''
-
-    {userName} (@{userLogin}), you are about to use the {spell} spell. Are you sure this is the right one?
-    If yes, then the secret code assosciated with it is:
-    {code}
-        ''')
-       
+    {userName} (@{userLogin}), you have found the right spell. As a part of your reward for solving the riddle,
+    here is the secret code assosciated with it: {code}
+    ''')
 
 except Exception as e:
     print("Unkown Error occured, please try again later")
